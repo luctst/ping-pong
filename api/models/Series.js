08674baa759f93d0f63a171ccf1794e51a8f4b3d
luctst <lucas.tostee@gmail.com>
2020-11-18 11:16:30 +0100

    Add some logic to Series Schema

--- api/models/Series.js
+++ api/models/Series.js
@@ -1,21 +1,64 @@
 const mongoose = require('mongoose');
+const playersModel = require('./Players');
 
-module.exports = mongoose.model(
-    'Series',
-    new mongoose.Schema({
-        date: {
-            type: Date
-        },
-        games: {
-            type: [{
-                players: [{
+const SeriesSchema = new mongoose.Schema({
+    date: {
+        type: String,
+        default: `${new Date().getDate()}/${new Date().getMonth() + 1}/${new Date().getFullYear()}`,
+        index: true
+    },
+    games: {
+        type: [{
+            _id: false,
+            players: {
+                winner: {
                     type: mongoose.Schema.Types.ObjectId,
                     ref: 'Players'
-                }],
-                winner: mongoose.Schema.Types.ObjectId,
-                score: [Number]
-            }],
-            default: []
-        }
-    }, {timestamps: false,  versionKey: false})
-);
+                },
+                looser: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: 'Players'
+                },
+            },
+            score: String
+        }],
+        default: [],
+        required: true
+    }
+}, { timestamps: false, versionKey: false });
+
+SeriesSchema.pre('find', function () {
+    this.populate('games.players.winner', 'name');
+    this.populate('games.players.looser', 'name');
+})
+
+const Series = mongoose.model('Series', SeriesSchema);
+
+Series.watch().on('change', async function (newData) {
+    if (newData.operationType === 'update') {
+        const fieldUpdate = Object.keys(newData.updateDescription.updatedFields)[0];
+        const playersSession = await playersModel
+            .where('_id').in([
+                mongoose.Types.ObjectId(newData.updateDescription.updatedFields[fieldUpdate].players.winner),
+                mongoose.Types.ObjectId(newData.updateDescription.updatedFields[fieldUpdate].players.looser)
+            ]);
+        
+        await Promise.all(playersSession.map(async p => {
+            const fieldsToUpdate = {
+                gamesPlayed: p.gamesPlayed + 1
+            };
+
+            if (!p.series.includes(newData.documentKey._id)) {
+                fieldsToUpdate.$push = {series: newData.documentKey._id}
+            }
+
+            if (newData.updateDescription.updatedFields[fieldUpdate].players.winner.toString() === p._id.toString()) {
+                fieldsToUpdate.win = p.win + 1;
+            };
+
+            await playersModel.updateOne({_id: mongoose.Types.ObjectId(p._id)}, { ...fieldsToUpdate });
+        }))
+    }
+});
+
+module.exports = Series;
