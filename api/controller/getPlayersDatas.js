ffed6645bd262b010b3ac9caa6faa5bd85c756ee
luctst <lucas.tostee@gmail.com>
2020-11-18 11:14:20 +0100

    Create controller who return data to user

---
+++ api/controller/getPlayersDatas.js
@@ -0,0 +1,14 @@
+const PlayersModel = require('../models/Players');
+
+module.exports = async function () {
+    const AllDatas = await PlayersModel.find({})
+        .populate('series')
+        .exec();
+    
+    const forClient = Object.keys(AllDatas).map(p => AllDatas[p]);
+
+    return {
+        code: 200,
+        modifyResponse: forClient
+    }
+}
