90a458fddcdd30d44c3cfa3c58c0fc4b1b953acf
luctst <lucas.tostee@gmail.com>
2020-11-18 11:13:40 +0100

    Add cors config

--- api/index.js
+++ api/index.js
@@ -1,24 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const compression = require('compression');
 
 const initDb = require('./middlewares/initDb');
 const playersRouter = require('./routes/players');
 
+app.use(cors({
+    origin: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '',
+    credentials: true
+}));
 app.use(compression());
-app.use(
-    cors({
-        origin: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '',
-        credentials: true,
-    })
-);
 app.use(express.json());
-app.use(cookieParser());
 app.use(initDb());
 
-app.use('/api/players', playersRouter);
+app.use('/api/player', playersRouter);
 
 app.listen(4000);
