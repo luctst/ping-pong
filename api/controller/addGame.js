507d54703cb5057adb08e083a7be03bc47eeed95
luctst <lucas.tostee@gmail.com>
2020-11-18 11:13:56 +0100

    Create controller to add a game

---
+++ api/controller/addGame.js
@@ -0,0 +1,37 @@
+const SeriesModel = require('../models/Series');
+
+/**
+ * Add a new game in a session
+ * @param {Object} data - 
+ */
+module.exports = async function (data) {
+    try {
+        const session = await SeriesModel.findOne({date: data.session}, 'games date').exec();
+    
+        if (!session) {
+            delete data.session;
+    
+            const newSession = new SeriesModel({
+                games: [{ ...data }]
+            })
+            
+            await newSession.save();
+            return {
+                code: 201,
+                modifyResponse: newSession._doc
+            }
+        }
+
+        session.games.push(data);
+        await session.save();
+
+        return {
+            code: 201,
+            modifyResponse: session._doc
+        }
+    } catch (error) {
+        return {
+            code: 500
+        }
+    }
+}
